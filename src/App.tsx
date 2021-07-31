532c2ebe6223a168b26af1f09b2b5d3cffad547f
Mark Morcos <mark.yehia@gmail.com>
2021-07-31 17:39:39 +0200

    Initial skeleton

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import * as React from 'react';
+
+interface Props { }
+
+class App extends React.Component<Props> {
+  render() {
+    return (
+      <>
+        <h1>
+          Hello, world!
+        </h1>
+      </>
+    );
+  }
+}
+
+export default App;
