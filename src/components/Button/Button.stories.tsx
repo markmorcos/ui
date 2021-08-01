f84a954e55a6347a327c4112112e0049becf36cf
Mark Morcos <mark.yehia@gmail.com>
2021-08-01 04:46:34 +0200

    Add onClick events for Button stories

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -15,15 +15,18 @@ export const Primary = Template.bind({})
 Primary.args = {
   primary: true,
   children: 'Primary',
+  onClick: () => alert('Primary'),
 }
 
 export const Secondary = Template.bind({})
 Secondary.args = {
   children: 'Secondary',
+  onClick: () => alert('Secondary'),
 }
 
 export const Disabled = Template.bind({})
 Disabled.args = {
   disabled: true,
   children: 'Disabled',
+  onClick: () => alert('Disabled'),
 }
