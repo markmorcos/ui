f9b943e8ce513248ad28a3b7f1d36f7005654f0d
Mark Morcos <mark.yehia@gmail.com>
2021-08-07 00:41:46 +0200

    Remove loki

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,7 @@ module.exports = {
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-postcss',
-    'loki/configure-react'
+    '@storybook/addon-postcss'
   ],
   webpackFinal: { performance: { hints: false } },
 }
