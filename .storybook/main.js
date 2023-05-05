4131293a4d8e9d7fd7b737d0cab142f3f044b75c
Mark Morcos <mark.yehia@gmail.com>
2023-05-06 01:49:29 +0200

    chore: switch to pnpm and upgrade dependencies to latest version

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,9 +1,16 @@
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: [
-    '@storybook/addon-links',
-    '@storybook/addon-essentials',
-    '@storybook/addon-postcss'
-  ],
-  webpackFinal: { performance: { hints: false } },
-}
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+  webpackFinal: {
+    performance: {
+      hints: false
+    }
+  },
+  framework: {
+    name: '@storybook/react-webpack5',
+    options: {}
+  },
+  docs: {
+    autodocs: true
+  }
+};
